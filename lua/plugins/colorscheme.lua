b2f6f716a76fb721be7400f63204c56b96590c4c
ossorio <jossorio-pro@outlook.com>
2024-04-20 20:26:02 -0400

    rebasing

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,68 +1,72 @@
 return {
-	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 20000 },
-	{ "rebelot/kanagawa.nvim" }, -- tokyonight
-	{
-		"folke/tokyonight.nvim",
-		lazy = true,
-		priority = 100,
-		opts = { style = "night" },
-	},
-	{
-		"tiagovla/tokyodark.nvim",
-		opts = {},
-		config = function(_, opts)
-			require("tokyodark").setup(opts) -- calling setup is optional
-			vim.cmd([[colorscheme tokyodark]])
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "tokyonight",
-		},
-	},
-	-- catppuccin
-	{
-		"catppuccin/nvim",
-		lazy = true,
-		name = "catppuccin",
-		priority = 100,
-		opts = {
-			integrations = {
-				aerial = true,
-				alpha = true,
-				cmp = true,
-				dashboard = true,
-				flash = true,
-				gitsigns = true,
-				headlines = true,
-				illuminate = true,
-				indent_blankline = { enabled = true },
-				leap = true,
-				lsp_trouble = true,
-				mason = true,
-				markdown = true,
-				mini = true,
-				native_lsp = {
-					enabled = true,
-					underlines = {
-						errors = { "undercurl" },
-						hints = { "undercurl" },
-						warnings = { "undercurl" },
-						information = { "undercurl" },
-					},
-				},
-				navic = { enabled = true, custom_bg = "lualine" },
-				neotest = true,
-				neotree = true,
-				noice = true,
-				notify = true,
-				semantic_tokens = true,
-				telescope = true,
-				treesitter = true,
-				treesitter_context = true,
-				which_key = true,
-			},
-		},
-	},
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 20000 },
+  { "rebelot/kanagawa.nvim" }, -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    priority = 100,
+    name = "tokyonight",
+    opts = { style = "night" },
+  },
+  {
+    "tiagovla/tokyodark.nvim",
+    opts = {},
+    config = function(_, opts)
+      require("tokyodark").setup(opts) -- calling setup is optional
+      vim.cmd([[colorscheme tokyodark]])
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+  -- catppucci
+  --
+  -- :wqa
+  -- n
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    priority = 100,
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
 }
