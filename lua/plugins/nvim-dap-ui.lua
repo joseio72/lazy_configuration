142f0e2e4340195da005d6153899d9bca08d1d7e
jOssorioRojas <jossorio-pro@outlook.com>
2024-05-22 09:36:09 -0400

    saving

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -1,6 +1,6 @@
 return {
 	"rcarriga/nvim-dap-ui",
-	dependencies = { "nvim-neotest/nvim-nio" },
+	dependencies = { "nvim-neotest/nvim-nio", "mfussenegger/nvim-dap" },
   -- stylua: ignore
   keys = {
     { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
